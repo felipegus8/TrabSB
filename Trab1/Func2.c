2e65a0137ce87d9c714e409e711ccad3001e599e
Felipe Viberti <felipegus8@yahoo.com.br>
2017-04-22 11:22:29 -0300

    Start utf16_8

--- Trab1/Func2.c
+++ Trab1/Func2.c
@@ -1,1 +1,18 @@
 
+
+#include <stdio.h>
+int utf16_8(FILE *arq_entrada, FILE *arq_saida) {
+    int first = fgetc(arq_entrada);
+    int second = fgetc(arq_entrada);
+    if (first != 0xfe || second != 0xff) {
+        fprintf(stderr, "%s", "BOM incorreto!\n");
+        return -1;
+    }
+    int startFile = fgetc(arq_entrada);
+    while (startFile != 0xffffffff) {
+        printf("firstbyte = %x\n",(unsigned)startFile);
+        startFile = fgetc(arq_entrada);
+    }
+    return 1;
+}
+
---
+++ Trab1/conv_utf.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int utf8_16(FILE *arq_entrada, FILE *arq_saida);
+int utf16_8(FILE *arq_entrada, FILE *arq_saida);
+
