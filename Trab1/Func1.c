4475b2d153ee0c868aac4ab9fd493dbdf4dde090
matheusroleal <matheusroleal@gmail.com>
2017-04-26 22:36:21 -0300

    Agora deve estar funcionando

--- Trab1/Func1.c
+++ Trab1/Func1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void dump (void *p, int n) {
-  unsigned char *p1 = p;
-  while (n--) {
-    printf("%p - %02x\n", p1, *p1);
-    p1++;
-  }
-}
+// void dump (void *p, int n) {
+//   unsigned char *p1 = p;
+//   while (n--) {
+//     printf("%p - %02x\n", p1, *p1);
+//     p1++;
+//   }
+// }
 
 
 int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
@@ -38,6 +39,12 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
   unsigned char v[4];
 
   while (i < tamanho ){
+
+    unsigned int masc, temp, binario;
+
+    unsigned short unit_1, unit_2,tmp_unit_1,tmp_unit_2;
+    unsigned int tmp;
+
     n = fread (v, 1, 1, arq_entrada);
     if(n!=1){
       fprintf(stderr,"Erro de Leitura 1!\n");
@@ -45,13 +52,30 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
     }
 
     if(((v[0]&0x80)>>7)==0){
+      // PARA 1 BYTE
       i++;
-      dump(&v[1],1);
+
+      // dump(&v[1],1);
+      masc = 0x7F;
+			binario = v[0] & masc;
+
+      //converte unit para utf16
+
+      unit_1 = binario;
+      unit_2 = 0x0;
 
     }else if(((v[0]&0x20)>>5)==0){
+      //PARA 2 BYTES
 
       n = fread ((v+1), 1, 1, arq_entrada);
-      dump(&v[1],1);
+      // dump(&v[1],1);
+
+      masc = 0x1F;
+      temp = v[0] & masc;
+      temp = temp << 6;
+      masc = 0x3F;
+      binario = v[1] & masc;
+      binario = binario + temp;
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 2!\n");
@@ -59,16 +83,22 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
       }
       i = i + 2;
 
+      //converte unit para utf16
+
+      unit_1 = binario;
+      unit_2 = 0x0;
+
     }else if(((v[0]&0x10)>>4)==0){
+      //PARA 3 BYTES
       n = fread ((v+1), 1, 1, arq_entrada);
-      dump(&v[1],1);
+      // dump(&v[1],1);
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 3!\n");
         return -1;
       }
       n = fread ((v+2), 1, 1, arq_entrada);
-      dump(&v[2],1);
+      // dump(&v[2],1);
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 4!\n");
@@ -76,10 +106,25 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
       }
       i = i + 3;
 
-    }else if(((v[0]&0x8)>> 3)==0){
+      masc = 0xF;
+			temp = v[0] & masc;
+			temp = temp << 12;
+			masc = 0x3F;
+			binario = v[1] & masc;
+			binario = binario << 6;
+			temp = temp + binario;
+			binario = v[2] & masc;
+			binario = temp + binario;
 
+      //converte unit para utf16
+
+      unit_1 = binario;
+      unit_2 = 0x0;
+
+    }else if(((v[0]&0x8)>> 3)==0){
+      //PARA 4 BYTES
       n = fread ((v+1), 1, 1, arq_entrada);
-      dump(&v[1],1);
+      // dump(&v[1],1);
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 5!\n");
@@ -87,7 +132,7 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
       }
 
       n = fread ((v+2), 1, 1, arq_entrada);
-      dump(&v[2],1);
+      // dump(&v[2],1);
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 6!\n");
@@ -95,7 +140,7 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
       }
 
       n = fread ((v+3), 1, 1, arq_entrada);
-      dump(&v[3],1);
+      // dump(&v[3],1);
 
       if(n!=1){
         fprintf(stderr,"Erro de Leitura 7!\n");
@@ -104,13 +149,64 @@ int utf8_16(FILE *arq_entrada, FILE *arq_saida) {
 
       i = i + 4;
 
+      //converte de utf8 para unicode
+
+      masc = 0x7;
+      temp = v[0] & masc;
+      temp = temp << 18;
+      masc = 0x3F;
+      binario = v[1] & masc;
+      binario = binario << 12;
+      temp = temp + binario;
+      binario = v[2] & masc;
+      binario = binario << 6;
+      temp = temp + binario;
+      binario = v[3] & masc;
+      binario = temp + binario;
+
+      //converte unit para utf16
+
+
+      tmp = binario - 0x10000;
+      tmp_unit_1 = (tmp & 0xFFC00) >> 10;
+      tmp_unit_1 = unit_1 + 0xD800;
+      tmp_unit_2 = tmp & 0x3FF;
+      tmp_unit_2 = unit_2 + 0xDC00;
+
+      unit_1 = tmp_unit_1;
+      unit_2 = tmp_unit_2;
+
     }else{
 
       printf ("Arquivo com erro de codificacao na posicao %d\n", i);
       return -1;
 
     }
+
+    tmp = (unit_1 & 0xFF) << 8;
+		unit_1 = (unit_1 & 0xFF00) >> 8;
+		unit_1 = unit_1 + tmp;
+
+    if(((v[0]&0x10)>>4)==0){
+      tmp = (unit_2& 0xFF) << 8;
+      unit_2= (unit_2& 0xFF00) >> 8;
+      unit_2= unit_2+ tmp;
+    }
+
+    n = fwrite (&unit_1, 2, 1, arq_saida);
+    if(n!=1){
+      fprintf(stderr,"Erro de Gravacao!\n");
+      return -1;
+    }
+    if(unit_2 != 0x0){
+
+  		n = fwrite (&unit_2, 2, 1, arq_saida);
+
+      if(n!=1){
+        fprintf(stderr,"Erro de Gravacao!\n");
+        return -1;
+      }
+  	}
   }
   return 0;
 }
-
