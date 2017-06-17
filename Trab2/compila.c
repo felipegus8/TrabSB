9b46b399cf2c231027c23a4df4b159d266b7a54b
Felipe Viberti <felipegus8@yahoo.com.br>
2017-06-17 18:43:57 -0300

    Função de atribuição feita e função do final do código

--- Trab2/compila.c
+++ Trab2/compila.c
@@ -66,6 +66,21 @@ void atribuicao(FILE *myfp, int line, int c,Memory *block){
         block->nextFree ++;
       }
       case 'v':
+      //No caso de uma variável local,ela tem que estar na pilha.Só tem que descobrir em que posição.
+      //Os três primeiros códigos são iguais.
+      //Aqui está movendo um valor da pilha para %r12d.
+      block->code[block->nextFree] = 0x44;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0x8B;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0x65;
+      block->nextFree ++;
+
+      //O quarto muda de acordo com o número da variável,que indica onde ela está na pilha.
+      unsigned char local_pilha = 0xFC - (var1 * 4);
+      block->code[block->nextFree] = local_pilha;
+      block->nextFree ++;
+
 
       case '$':
       //movl $const,%r12d
@@ -106,6 +121,18 @@ void atribuicao(FILE *myfp, int line, int c,Memory *block){
      block->nextFree ++;
     }
     case 'v':
+    //Mesmo caso do primeiro switch.Só muda aqui é que está movendo para %r13d.
+    block->code[block->nextFree] = 0x44;
+    block->nextFree ++;
+    block->code[block->nextFree] = 0x8B;
+    block->nextFree ++;
+    block->code[block->nextFree] = 0x6D;
+    block->nextFree ++;
+
+    //O quarto muda de acordo com o número da variável,que indica onde ela está na pilha.
+    unsigned char local_pilha = 0xFC - (var2 * 4);
+    block->code[block->nextFree] = local_pilha;
+    block->nextFree ++;
 
     case '$':
     //movl $const,%r13d
@@ -123,7 +150,56 @@ void atribuicao(FILE *myfp, int line, int c,Memory *block){
     block->code[block->nextFree] = (char) idx1 >> 24;
     block->nextFree ++;
   }
+
+  switch (op) {
+    //Três casos.Add,Sub e Mul,todos de %r13d em %r12d
+    case '+':
+      // 45 01 ec     add %r13d,%r12d
+      block->code[block->nextFree] = 0x45;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0x01;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0xEC;
+      block->nextFree ++;
+
+    case '-':
+      //45 29 EC   sub %r13d,%r12d
+      block->code[block->nextFree] = 0x45;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0x29;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0xEC;
+      block->nextFree ++;
+
+    case '*':
+      //45 of af e5 imul %r13d,%r12d
+      block->code[block->nextFree] = 0x45;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0x0F;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0xAF;
+      block->nextFree ++;
+      block->code[block->nextFree] = 0xE5;
+      block->nextFree ++;
   }
+  }
+
+  //Agora tem que colocar o que está em %r12d no lugar certo na pilha.
+  //Só o que muda no código de máquina de uma posição da pilha para outra são os dois últimos dígitos que variam de acordo com o lugar onde a variavel está sendo colocada.
+  //O início sempre é 44 89 65
+
+  block->code[block->nextFree] = 0x44;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0x89;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0x65;
+  block->nextFree ++;
+
+  //O maior código de máquina é o da primeira posição.(0xFC).Ele diminui de 4 a cada próxima posição.
+  unsigned char local_pilha = 0xFC - (var0 * 4);
+  block->code[block->nextFree] = local_pilha;
+  block->nextFree ++;
+
 }
 
 
@@ -142,7 +218,7 @@ void init_func (Memory *block) {
   unsigned char init[8];
   int i;
   //0:	55                   	push   %rbp
-  init[0] = 0x55
+  init[0] = 0x55;
 
 
   //1:	48 89 e5             	mov    %rsp,%rbp
@@ -150,11 +226,12 @@ void init_func (Memory *block) {
   init[2] = 0x89;
   init[3] = 0xE5;
 
-  //4:	48 83 ec 20          	sub    $0x20,%rsp
+  //4:	48 83 ec 10          	sub    $0x10,%rsp
+  //Precisa de 16 na pilha para armazenar as 4 variáveis locais.
   init[4] = 0x48;
   init[5] = 0x83;
   init[6] = 0xEC;
-  init[7] = 0x20;
+  init[7] = 0x10;
 
   for (i=0;i<8;i++) {
     block->code[block->nextFree] = init[i];
@@ -162,6 +239,23 @@ void init_func (Memory *block) {
   }
 }
 
+void end_code (Memory *block) {
+  //Instruções para finalizar o programa
+  //mov %rbp,%rsp   48 89 EC
+  //pop %rbo   5D
+  //retq  C3
+  block->code[block->nextFree] = 0x48;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0x89;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0xEC;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0x5D;
+  block->nextFree ++;
+  block->code[block->nextFree] = 0xC3;
+  block->nextFree ++;
+
+}
 Memory *init_memory() {
   Memory *init;
   init = (Memory*)malloc (sizeof(Memory));
